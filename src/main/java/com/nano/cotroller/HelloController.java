fba9f01c3c5544091113d1864684f7403e0adca9
Ahjsk <17600455713@163.com>
2026-04-26 15:15:08 +0800

    add HelloController

---
+++ src/main/java/com/nano/cotroller/HelloController.java
@@ -0,0 +1,13 @@
+package com.nano.cotroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
